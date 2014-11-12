3ec38fbea6827bcac352c18d4acce7d2aa3ce6ff
Marc Boquet <marc@apalan.cat>
2014-11-12 11:52:22 +0100

    Initial Commit

---
+++ WhereTheFIAm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhereTheFIAm
+//
+//  Created by Marc Boquet Bertran on 12/11/14.
+//  Copyright (c) 2014 Marc Boquet Bertran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhereTheFIAm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhereTheFIAm
+//
+//  Created by Marc Boquet Bertran on 12/11/14.
+//  Copyright (c) 2014 Marc Boquet Bertran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
